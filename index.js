1fdffd248ff510c6aca7d9b0f5466d4f14c497e4
Lmfarr <lmfarr01@gmail.com>
2018-02-21 21:04:34 -0500

    sticky nav test

--- index.js
+++ index.js
@@ -179,3 +179,25 @@ jQuery(function($) {
     filter: '.smoothScroll'
   });
 });
+
+
+
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var nav = document.getElementById("nav");
+
+// Get the offset position of the navbar
+var sticky = nav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
