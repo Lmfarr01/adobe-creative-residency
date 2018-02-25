d509fa1cd34541ea70cf3df15e9c5174dca42282
Lmfarr <lmfarr01@gmail.com>
2018-02-25 13:03:25 -0500

    html update 1

--- index.js
+++ index.js
@@ -1,203 +1,133 @@
-if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
+<html>
+  <head>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
 
-;
-(function($) {
-  var h = $.scrollTo = function(a, b, c) {
-    $(window).scrollTo(a, b, c)
-  };
-  h.defaults = {
-    axis: 'xy',
-    duration: parseFloat($.fn.jquery) >= 1.3 ? 0 : 1,
-    limit: true
-  };
-  h.window = function(a) {
-    return $(window)._scrollable()
-  };
-  $.fn._scrollable = function() {
-    return this.map(function() {
-      var a = this,
-        isWin = !a.nodeName || $.inArray(a.nodeName.toLowerCase(), ['iframe', '#document', 'html', 'body']) != -1;
-      if (!isWin) return a;
-      var b = (a.contentWindow || a).document || a.ownerDocument || a;
-      return /webkit/i.test(navigator.userAgent) || b.compatMode == 'BackCompat' ? b.body : b.documentElement
-    })
-  };
-  $.fn.scrollTo = function(e, f, g) {
-    if (typeof f == 'object') {
-      g = f;
-      f = 0
-    }
-    if (typeof g == 'function') g = {
-      onAfter: g
-    };
-    if (e == 'max') e = 9e9;
-    g = $.extend({}, h.defaults, g);
-    f = f || g.duration;
-    g.queue = g.queue && g.axis.length > 1;
-    if (g.queue) f /= 2;
-    g.offset = both(g.offset);
-    g.over = both(g.over);
-    return this._scrollable().each(function() {
-      if (e == null) return;
-      var d = this,
-        $elem = $(d),
-        targ = e,
-        toff, attr = {},
-        win = $elem.is('html,body');
-      switch (typeof targ) {
-        case 'number':
-        case 'string':
-          if (/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)) {
-            targ = both(targ);
-            break
-          }
-          targ = $(targ, this);
-          if (!targ.length) return;
-        case 'object':
-          if (targ.is || targ.style) toff = (targ = $(targ)).offset()
-      }
-      $.each(g.axis.split(''), function(i, a) {
-        var b = a == 'x' ? 'Left' : 'Top',
-          pos = b.toLowerCase(),
-          key = 'scroll' + b,
-          old = d[key],
-          max = h.max(d, a);
-        if (toff) {
-          attr[key] = toff[pos] + (win ? 0 : old - $elem.offset()[pos]);
-          if (g.margin) {
-            attr[key] -= parseInt(targ.css('margin' + b)) || 0;
-            attr[key] -= parseInt(targ.css('border' + b + 'Width')) || 0
-          }
-          attr[key] += g.offset[pos] || 0;
-          if (g.over[pos]) attr[key] += targ[a == 'x' ? 'width' : 'height']() * g.over[pos]
-        } else {
-          var c = targ[pos];
-          attr[key] = c.slice && c.slice(-1) == '%' ? parseFloat(c) / 100 * max : c
-        }
-        if (g.limit && /^\d+$/.test(attr[key])) attr[key] = attr[key] <= 0 ? 0 : Math.min(attr[key], max);
-        if (!i && g.queue) {
-          if (old != attr[key]) animate(g.onAfterFirst);
-          delete attr[key]
-        }
-      });
-      animate(g.onAfter);
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adobe Creative Residency 2018</title>
+    
+        <link rel="stylesheet" href="style.css">
+        <script src="js/index.js"></script>
+    
+        <link href="https://fonts.googleapis.com/css?family=Krona+One" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Numans' rel='stylesheet' type='text/css'>
+    
+  </head>
 
-      function animate(a) {
-        $elem.animate(attr, f, g.easing, a && function() {
-          a.call(this, e, g)
-        })
-      }
-    }).end()
-  };
-  h.max = function(a, b) {
-    var c = b == 'x' ? 'Width' : 'Height',
-      scroll = 'scroll' + c;
-    if (!$(a).is('html,body')) return a[scroll] - $(a)[c.toLowerCase()]();
-    var d = 'client' + c,
-      html = a.ownerDocument.documentElement,
-      body = a.ownerDocument.body;
-    return Math.max(html[scroll], body[scroll]) - Math.min(html[d], body[d])
-  };
+<body>
+  <div id="section">
+    <div class="intro">
+    <font>Resin</font>
+    <p>xxx</p>
+  </div>
+    <div class="container">
+      <div class="nav">
+        <a href="#codeword" class="smoothScroll">GOALS</a>
+      </div>
+      <div class="nav2">
+        <a href="#banana" class="smoothScroll">TIMELINE</a>
+      </div>
+      <div class="nav3">
+        <a href="#apple" class="smoothScroll">INSPIRATION</a>
+      </div>
+      <div class="nav4">
+        <a href="#pear" class="smoothScroll">COMMUNITY</a>
+      </div>
+    </div>
+    <a href="#go" class="smoothScroll"><span class="arrow"></span></a>
+  </div>  
+  
+  
+  <div id="section1">
+    <div class="getting-there">
+      <h1>GOALS</h1>
+      <p>Fernwood Farm is a historic 6 acre property <br>outside of Cazenovia, New York. Built in <br>1874 by a member of the Linklaen family to <br>raise Guernsey Cattle, its main house sat <br>in disrepair until renovation efforts prepared <br>it for its
+      new life as a Bed & Breakfast. <br><br>We're excited to share this day with you and with <br>Fernwood Farm as their very first wedding!</p>
+      <a href="http://cdn0.dailydot.com/uploaded/images/original/2014/8/18/pusheen.gif">MAP</a>
+    </div>
+    <a href="#go1" class="smoothScroll"><span class="arrow2"></span></a>
+  </div>
 
-  function both(a) {
-    return typeof a == 'object' ? a : {
-      top: a,
-      left: a
-    }
-  }
-})(jQuery);
+  
+  <div id="section2">
+    <div class="lodging">
+    <h1>LODGING</h1>
+  </div>
+    <div class="container2">
+      <div class="lodging-p-1">
+        <p>We've booked rooms at the Syracuse Courtyard in Armory Square in the city where we met.</p>
+        <a href="http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Farrell%20/%20Nicholson%20Wedding%5Esyrdt%60FNWFNWA%7CFNWFNWB%60189%60USD%60false%604%609/30/16%6010/2/16%608/30/16&app=resvlink&stop_mobi=yes">BOOK</a>
+      </div>
+      <div class="lodging-p-2">
+        <p>Blocked rooms are available to book at the new Cazenovia Hampton Inn beginning in May.</p>
+        <a href="">WAIT</a>
+      </div>
+      <div class="lodging-p-3">
+        <p>Want a mini party with the big one? We've found historic homes to rent for the weekend.</p>
+        <a href="https://docs.google.com/document/d/1UVW0dc6XRY728PsV34jx7Aj3UUTZnaDnAuX79D_7vfM/edit?usp=sharing">VIEW</a>
+      </div>
+    </div>
+    <a href="#go2" class="smoothScroll"><span class="arrow3"></span></a>
+  </div>
+  
+  
+  <div id="section3">
+    <div class="registry">
+    <h1>REGISTRY</h1>
+  </div>
+    <div class="container3">
+      <div class="registry-p-1">
+        <p>We have our hound dog thanks to this rescue. We really like what they do and think you will too. </p>
+        <a href="http://www.badassbrooklynanimalrescue.com/">DONATE</a>
+      </div>
+      <div class="registry-p-2">
+        <p>Fancy helping us go on an adventure? You can contribute to our honeymoon here.</p>
+        <a href="http://cat-bounce.com/">CONTRIBUTE</a>
+      </div>
+      <div class="registry-p-3">
+        <p>If you'd like to spoil us, here are a few things from one of our favorite brooklyn stores.</p>
+        <a href="https://secure.westelm.com/registry/5tsnpksr8j/registry-list.html">GIFT</a>
+      </div>
+    </div>
+    <a href="#go3" class="smoothScroll"><span class="arrow4"></span></a>
+  </div>
 
-;
-(function(b) {
-  function g(a, e, d) {
-    var h = e.hash.slice(1),
-      f = document.getElementById(h) || document.getElementsByName(h)[0];
-    if (f) {
-      a && a.preventDefault();
-      var c = b(d.target);
-      if (!(d.lock && c.is(":animated") || d.onBefore && !1 === d.onBefore(a, f, c))) {
-        d.stop && c._scrollable().stop(!0);
-        if (d.hash) {
-          var a = f.id == h ? "id" : "name",
-            g = b("<a> </a>").attr(a, h).css({
-              position: "absolute",
-              top: b(window).scrollTop(),
-              left: b(window).scrollLeft()
-            });
-          f[a] = "";
-          b("body").prepend(g);
-          location = e.hash;
-          g.remove();
-          f[a] = h
-        }
-        c.scrollTo(f, d).trigger("notify.serialScroll", [f])
-      }
-    }
-  }
-  var i = location.href.replace(/#.*/, ""),
-    c = b.localScroll = function(a) {
-      b("body").localScroll(a)
-    };
-  c.defaults = {
-    duration: 1E3,
-    axis: "y",
-    event: "click",
-    stop: !0,
-    target: window,
-    reset: !0
-  };
-  c.hash = function(a) {
-    if (location.hash) {
-      a = b.extend({}, c.defaults, a);
-      a.hash = !1;
-      if (a.reset) {
-        var e = a.duration;
-        delete a.duration;
-        b(a.target).scrollTo(0, a);
-        a.duration = e
-      }
-      g(0, location, a)
-    }
-  };
-  b.fn.localScroll = function(a) {
-    function e() {
-      return !!this.href && !!this.hash && this.href.replace(this.hash, "") == i && (!a.filter || b(this).is(a.filter))
-    }
-    a = b.extend({}, c.defaults, a);
-    return a.lazy ? this.bind(a.event, function(d) {
-      var c = b([d.target, d.target.parentNode]).filter(e)[0];
-      c && g(d, c, a)
-    }) : this.find("a,area").filter(e).bind(a.event, function(b) {
-      g(b, this, a)
-    }).end().end()
-  }
-})(jQuery);
+  
+  <div id="section4">
+    <div class="soon">
+    <img src="">
+    <h1>COMING SOON</h1>
+    <p>Schedule <br>Wedding Party <br>Activities</p>
+    <a href="http://cat-bounce.com/">KILL TIME</a>
+  </div>
+  </div>
 
-// Initialize all .smoothScroll links
-jQuery(function($) {
-  $.localScroll({
-    filter: '.smoothScroll'
-  });
-});
+  <div class="navcode">
+    <a name="codeword">
+  </div>
+  <div class="navcode2">
+    <a name="banana">
+  </div>
+  <div class="navcode3">
+    <a name="apple">
+  </div>
+  <div class="navcode4">
+    <a name="pear">
+  </div>
+    
+  <div class="navcode">
+    <a name="go">
+  </div>
+  <div class="navcode2">
+    <a name="go1">
+  </div>
+  <div class="navcode3">
+    <a name="go2">
+  </div>
+  <div class="navcode4">
+    <a name="go3">
+  </div>
 
-
-
-
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var nav = document.getElementById("nav");
-
-// Get the offset position of the navbar
-var sticky = nav.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add("sticky")
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
+</body>
